bcc775f3fc639192aa791a90da8f56367573dbfd
DJ Picard <picard.daniel.cet@gmail.com>
2021-03-31 00:13:22 -0400

    updated rust rocket

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
-#[macro_use] extern crate rocket;
+use rocket::{self, get, routes};
 
+/// Declare a handler.
 #[get("/")]
 fn index() -> &'static str {
     "Hello, world!"
 }
 
+/// Start our server.
 fn main() {
     rocket::ignite().mount("/", routes![index]).launch();
 }
